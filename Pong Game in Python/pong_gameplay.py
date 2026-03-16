80c3f5dd45d4e634e4725cc54734c4b9c080f86d
Lukasz <146345005+LukaszCode@users.noreply.github.com>
2026-03-16 17:45:25 +0000

    Refactor paddle movement to use delta time
    Updated paddle movement function to include delta time for smoother movement.

--- Pong Game in Python/pong_gameplay.py
+++ Pong Game in Python/pong_gameplay.py
@@ -15,30 +15,35 @@ WINDOW = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Simple_Pong_Game")
 FPS = 60
 
-def handle_key_paddle_movement(keys, left_paddle, right_paddle):
+def handle_key_paddle_movement(keys, left_paddle, right_paddle, dt):
     if keys[pygame.K_w]:
-        left_paddle.move(up=True, height=HEIGHT)
+        left_paddle.move(up=True, height=HEIGHT, dt=dt)
     elif keys[pygame.K_s]:
-        left_paddle.move(up=False, height=HEIGHT)
+        left_paddle.move(up=False, height=HEIGHT, dt=dt)
 
     if keys[pygame.K_UP]:
-        right_paddle.move(up=True, height=HEIGHT)
+        right_paddle.move(up=True, height=HEIGHT, dt=dt)
     elif keys[pygame.K_DOWN]:
-        right_paddle.move(up=False, height=HEIGHT)
-
+        right_paddle.move(up=False, height=HEIGHT, dt=dt)
 
 
 def main():
     run = True
+    left_score = 0
+    right_score = 0
 
     def reset_game():
         """Resets the ball and paddles to their initial positions."""
-        return Paddle(10, HEIGHT // 2 - 50, 20, 100), Paddle(WIDTH - 30, HEIGHT // 2 - 50, 20, 100), Ball(WIDTH // 2, HEIGHT // 2, 10)
+        left_paddle = Paddle(10, HEIGHT // 2 - 50, 20, 100)
+        right_paddle = Paddle(WIDTH - 30, HEIGHT // 2 - 50, 20, 100) 
+        ball = Ball(WIDTH // 2, HEIGHT // 2, 10)
+        return left_paddle, right_paddle, ball
 
     left_paddle, right_paddle, ball = reset_game()
 
     while run:
-        clock.tick(FPS)
+        """Give time in seconds"""
+        dt = clock.tick(FPS) / 1000 
         draw_window(WINDOW, [left_paddle, right_paddle], ball, WIDTH, HEIGHT)
 
         for event in pygame.event.get():
